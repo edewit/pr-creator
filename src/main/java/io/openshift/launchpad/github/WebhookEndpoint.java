c009f3be93058235ba58962709a544ad931e2820
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-07-06 10:53:35 +0200

    added logging

--- src/main/java/io/openshift/launchpad/github/WebhookEndpoint.java
+++ src/main/java/io/openshift/launchpad/github/WebhookEndpoint.java
@@ -12,6 +12,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+
 import io.openshift.booster.catalog.Booster;
 import io.openshift.booster.catalog.BoosterCatalogService;
 import io.openshift.launchpad.github.model.Mapping;
@@ -24,6 +26,9 @@ import io.openshift.launchpad.github.model.PullRequest;
 @ApplicationScoped
 public class WebhookEndpoint {
 
+	@Inject
+	private Logger logger;
+	
     @Inject
     private PullRequestService pullRequest;
 
@@ -43,14 +48,21 @@ public class WebhookEndpoint {
     @Path("/hook")
     @Consumes(MediaType.APPLICATION_JSON)
     public void hook(PullRequest payload) throws IOException {
+    	logger.info("Analising PR for booster documentation update ({})", payload);
         Mapping mapping = pullRequest.isDocumentationUpdated(payload.getRepository().getFullName(), payload.getNumber());
+        
         if (mapping != null) {
+        	logger.info("this PR updates mission {}", mapping.getMissionName());
             File location = pullRequest.fork(boosters, mapping.getMissionName());
+            logger.debug("forked boosters {}", (Object)location.list());
             File documentationFolder = pullRequest.checkout(payload.getRepository().getFullName());
+            logger.debug("doc checkout {}", (Object)documentationFolder.list());
             String html = templateMergerService.convertToAsciidoc(new File(documentationFolder, mapping.getDocumentationLocation()));
             File file = templateMergerService.mergeTemplate(location, html);
             file.renameTo(location.listFiles(name -> name.getName().equals(file.getName()))[0]);
             pullRequest.createPullRequest(location);
+        } else {
+        	logger.info("no booster documentation changes found");
         }
     }
 }
